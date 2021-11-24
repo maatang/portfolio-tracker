c9ad46be0316988a613d7e05b489c273b854f185
maatang <maatang95@gmail.com>
2021-11-24 16:06:35 -0500

    Add endpoint to get current crypto price

--- asset_api/urls.py
+++ asset_api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('hello/', views.say_hello), ]
+urlpatterns = [path('hello/', views.sayHello),
+               path('cryptoprice/<str:coinTicker>', views.getCryptoPrice)]
--- asset_api/views.py
+++ asset_api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 
-def say_hello(request):
+def sayHello(request):
     return HttpResponse('Hello World')
+
+
+def getCryptoPrice(request, coinTicker):
+    usdPair = coinTicker + 'USD'
+    response = requests.get(
+        'https://api.kraken.com/0/public/Ticker?pair=%s' % usdPair)
+    return HttpResponse(response)
